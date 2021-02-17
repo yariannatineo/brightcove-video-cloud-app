80ad569622d2d8e64263cf6738e208abe034303e
Yarianna Tineo <yariannactineop@gmail.com>
2021-02-17 11:30:13 -0600

    config port

--- client/src/api/myVideoApi.js
+++ client/src/api/myVideoApi.js
@@ -1,4 +1,4 @@
 //  axios import
 import axios from 'axios';
 // api instance
-export default axios.create({ baseURL: 'http://localhost:8000/api' });
+export default axios.create({ baseURL: 'http://localhost:8080/api' });
