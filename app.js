b7f4c4d2e7cd24c96562064a2f1e407b53cf71a2
Yarianna Tineo <yariannactineop@gmail.com>
2021-02-16 23:35:53 -0600

    heroku-postbuild and start script added 6

--- app.js
+++ app.js
@@ -38,21 +38,15 @@ app.get("/api", (req, res) => {
     res.status(200).json({ msg: "Hello" });
 });
 
-// ... other app.use middleware 
-// app.use(express.static(path.join(__dirname, "client", "build")))
-
 if (process.env.NODE_ENV === 'production') {
 
-    app.use(express.static('client/build'));
-   
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.use(express.static(path.join(__dirname, "client", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
 }
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+
 
 
 //  server process
