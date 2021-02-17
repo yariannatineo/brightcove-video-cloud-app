6144486f81ac030ad1d2446349db020ac3a6b02e
Yarianna Tineo <yariannactineop@gmail.com>
2021-02-17 10:55:17 -0600

    proxy setup

--- client/src/actions/playlist.js
+++ client/src/actions/playlist.js
@@ -1,15 +1,15 @@
 // import type
 import { FETCH_PLAYLISTS } from './types';
 // import api
-//import myVideoApi from '../api/myVideoApi';
-import axios from 'axios';
+import myVideoApi from '../api/myVideoApi';
+
 
 // method to get all playlists
 export const fetchPlaylists = () => async dispatch => {
 
     try {
         // request to api
-        const res = await axios.get('/playlist');
+        const res = await myVideoApi.get('/playlist');
         // send data to the reducer
         dispatch({ type: FETCH_PLAYLISTS , payload: res.data});
     // catch any errors
--- client/src/actions/video.js
+++ client/src/actions/video.js
@@ -1,14 +1,14 @@
 // import type
 import { FETCH_VIDEOS, FETCH_VIDEOS_MEXICO } from './types';
 // import api
-//import myVideoApi from '../api/myVideoApi';
-import axios from 'axios';
+import myVideoApi from '../api/myVideoApi';
+
 
 // get all videos
 export const fetchVideos = () => async dispatch => {
     try {
         // request to api
-        const res = await axios.get('/video');
+        const res = await myVideoApi.get('/video');
         // send data to the reducer
         dispatch({ type: FETCH_VIDEOS , payload: res.data});
     // catch errors
@@ -21,7 +21,7 @@ export const fetchVideos = () => async dispatch => {
 export const fetchVideosMexico = () => async dispatch => {
     try {
         // request to api
-        const res = await axios.get('/video/mexico');
+        const res = await myVideoApi.get('/video/mexico');
         // send data to the reducer
         dispatch({ type: FETCH_VIDEOS_MEXICO , payload: res.data});
     //  catch errors
