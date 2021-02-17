9f870e608711a13365c76550a669133cfd9290ca
Yarianna Tineo <yariannactineop@gmail.com>
2021-02-17 12:37:44 -0600

    setting proxy

--- client/src/actions/playlist.js
+++ client/src/actions/playlist.js
@@ -1,15 +1,15 @@
 // import type
 import { FETCH_PLAYLISTS } from './types';
 // import api
-import myVideoApi from '../api/myVideoApi';
-
+//import myVideoApi from '../api/myVideoApi';
+import axios from 'axios';
 
 // method to get all playlists
 export const fetchPlaylists = () => async dispatch => {
 
     try {
         // request to api
-        const res = await myVideoApi.get('/playlist');
+        const res = await axios.get('/api/playlist');
         // send data to the reducer
         dispatch({ type: FETCH_PLAYLISTS , payload: res.data});
     // catch any errors
