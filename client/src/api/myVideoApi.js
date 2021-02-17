cff986b48e92790b41d76085e8a740960aa8a11c
Yarianna Tineo <yariannactineop@gmail.com>
2021-02-17 12:02:49 -0600

    config heroku urls and port

--- client/src/api/myVideoApi.js
+++ client/src/api/myVideoApi.js
@@ -1,4 +1,4 @@
 //  axios import
 import axios from 'axios';
 // api instance
-export default axios.create({ baseURL: 'http://localhost:8080/api' });
+export default axios.create({ baseURL: 'https://bc-video-cloud-app.herokuapp.com:8080/api' });
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
     app.use(createProxyMiddleware('/api', { 
-		target: 'http://localhost:8080',
+		target: 'https://bc-video-cloud-app.herokuapp.com:8080',
       	changeOrigin: true
 	}
    )
